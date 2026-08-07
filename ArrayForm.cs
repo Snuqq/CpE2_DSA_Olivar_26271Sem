f4211cc20293b323bc24bf7cc9b92289a0cc029f
Snuqq <josholivar62@gmail.com>
2026-08-07 14:02:08 +0800

    nice one

--- ArrayForm.cs
+++ ArrayForm.cs
@@ -27,7 +27,7 @@ namespace CpE2_DSA_Olivar_26271Sem
             }
             catch
             {
-                MessageBox.Show("Invalid input.");h
+                MessageBox.Show("Invalid input.");
                 txtIndex.Focus();
                 txtIndex.SelectAll();
             }
